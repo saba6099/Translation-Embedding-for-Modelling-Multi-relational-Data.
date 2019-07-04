51706b4b0d7c8c652dea9e55f34b82c38a40ca60
saba6099 <droptosabakhan@gmail.com>
2019-07-04 19:18:21 +0200

    Added model code in test.py

--- dump_of_test.py
+++ dump_of_test.py
@@ -16,67 +16,9 @@ from pyspark import SparkContext, SparkConf
 from bigdl.nn.keras.layer import Embedding
 init_engine()
 import os
-
-from bigdl.nn.layer import *
-from bigdl.nn.criterion import *
-from bigdl.optim.optimizer import *
-
-from pyspark import SparkContext, SparkConf
 os.environ["PYSPARK_PYTHON"]="/usr/local/bin/python3"
 
 
-def create_model(total_embeddings, embedding_dim = 2, margin=1.0):
-    model = Sequential()
-    model.add(Reshape([6]))
-    embedding = LookupTable(total_embeddings, embedding_dim)
-    model.add(embedding)
-    model.add(Reshape([2, 3, 1, 2])).add(Squeeze(1))
-    # print(model.forward((train_data)))
-    model.add(SplitTable(1))
-
-    branches = ParallelTable()
-    branch1 = Sequential()
-    pos_h_l = Sequential().add(ConcatTable().add(Select(1, 1)).add(Select(1, 3)))
-    pos_add = pos_h_l.add(CAddTable())
-    pos_t = Sequential().add(Select(1, 2)).add(MulConstant(-1.0))
-    triplepos_meta = Sequential().add(ConcatTable().add(pos_add).add(pos_t))
-    triplepos_dist = triplepos_meta.add(CAddTable()).add(Abs())
-    triplepos_score = triplepos_dist.add(Unsqueeze(1)).add(Mean(3, 1)).add(MulConstant(float(embedding_dim)))
-    branch1.add(triplepos_score)  # .add(AddConstant(1.0))#.add(Unsqueeze(1))
-    # pos_t= Sequential().add(Select(1,2)).add(MulConstant(-1.0))
-    # a = Sequential().add(Narrow(1,1,2)).add(SplitTable(2))
-    # c = Sequential().add(CAveTable())
-    # b = Sequential().add(Select(1,2))
-
-    # branch1 = Sequential().add(CAveTable()).add(MulConstant(1.0))
-    # branch1 = Sequential().add(c)
-
-    branch2 = Sequential()
-    neg_h_l = Sequential().add(ConcatTable().add(Select(1, 1)).add(Select(1, 3)))
-    neg_add = neg_h_l.add(CAddTable())
-    neg_t = Sequential().add(Select(1, 2)).add(MulConstant(-1.0))
-    tripleneg_meta = Sequential().add(ConcatTable().add(neg_add).add(neg_t))
-    tripleneg_dist = tripleneg_meta.add(CAddTable()).add(Abs())
-    tripleneg_score = tripleneg_dist.add(Unsqueeze(1)).add(Mean(3, 1)).add(MulConstant(float(embedding_dim)))
-    branch2.add(tripleneg_score)  # .add(Unsqueeze(1))
-    # pos_add= branch1.add(CAddTable())
-    # branch2 = Sequential().add(Narrow(1, 2))
-
-    # pos_h_l = Sequential().add(ConcatTable().add(Select(2,1)).add(Select(2,3)))
-    # pos_add= pos_h_l.add(CAddTable())
-    # branch1.add(pos_add)
-
-    #
-    # branch2 = Sequential().add(Select(2,2))
-    branches.add(branch1).add(branch2)
-    model.add(branches)
-    # model.add(CAveTable())
-    # print(model.forward(train_data))
-    # model.add(a)
-    pos_plus_margin = Sequential().add(SelectTable(1)).add(AddConstant(margin))
-
-    model.add(ConcatTable().add(pos_plus_margin).add(SelectTable(2))).add(CSubTable()).add(Abs())
-    return model
 
 
 class TransE:
@@ -103,11 +45,33 @@ class TransE:
         self.entity = []
         self.relation = []
         self.triple_embeddings = []
-        self.batch_total = []
 
+    def sample(self, size):
+        return sample(self.triplets_list, size)
 
+    def makeRDD(self, sc):
+        #
+        # data = (np.concatenate((self.batch_pos, self.batch_neg), axis = 1))
+        # print(data)
+        train_rdd = sc.parallelize(self.triple_embeddings)
+        print(train_rdd.collect())
+        self.all_heads_true = train_rdd.map(lambda x: (x[0]))
+        self.all_tails_true = train_rdd.map(lambda x: (x[1]))
+        self.all_relations_true = train_rdd.map(lambda x: (x[2]))
+        self.all_heads_corrupt = train_rdd.map(lambda x: (x[3]))
+        self.all_tails_corrupt = train_rdd.map(lambda x: (x[4]))
+        self.all_relations_corrupt = train_rdd.map(lambda x: (x[5]))
 
-    def generate_corrupted_triplets(self, cycle_index=1):
+        print(self.all_relations_corrupt.collect())
+        print("hello")
+        #layer = LookupTable(9, 4, 2.0, 0.1, 2.0, True)
+        #input = (np.array[train_rdd.collect()]).astype("float32")
+
+        #output = layer.forward(input)
+        #gradInput = layer.backward(input, output)
+        #print(output)
+        #print(gradInput)
+    def transE(self, cycle_index=20):
         count = 0
         print("\n********** Start TransE training **********")
         for i in range(cycle_index):
@@ -115,9 +79,8 @@ class TransE:
             if count == 0:
                 start_time = time.time()
             count += 1
-            print("Batch number:", count)
-            Sbatch = self.triplets_list
-            #Sbatch=self.
+            print("Epoch number:", count)
+            Sbatch = self.sample(4)
 
             raw_batch = Sbatch
             if raw_batch is None:
@@ -125,7 +88,7 @@ class TransE:
             else:
                 batch_pos = raw_batch
                 batch_neg = []
-                batch_total = []
+
                 for head, tail, relation in batch_pos:
                     corrupt_head_prob = np.random.binomial(1, 0.5)
                     head_neg = head
@@ -137,14 +100,11 @@ class TransE:
                             tail_neg = random.choice(list(self.entity_dict.values()))
                         if (head_neg, tail_neg, relation) not in (self.training_triple_pool and self.batch_neg):
                             break
-                    batch_neg = [[head_neg], [tail_neg], [relation]]
-                    batch_pos = [[head],[tail],[relation]]
-                    batch_total.append([batch_pos, batch_neg])
+                    batch_neg.append((head_neg, tail_neg, relation))
             self.batch_neg += batch_neg
             self.batch_pos += batch_pos
-            self.batch_total+=(batch_total)
-        print(("batch_pos",self.batch_pos))
-        print(("batch_neg",self.batch_neg))
+        print((self.batch_pos))
+        print((self.batch_neg))
 
     def create_embeddings(self):
         self.entity  = list((self.entity_dict.values()))
@@ -222,49 +182,25 @@ class TransE:
 
 
 
-    def make_samples(self,total_embeddings):
-        sample = np.array(self.batch_total)
-        sample_rdd = sc.parallelize(sample)
-        labels = np.zeros(len(self.triplets_list))
-        labels = sc.parallelize(labels)
-        record = sample_rdd.zip(labels)
-        train_data = record.map(lambda t: Sample.from_ndarray(t[0], t[1]))
-        print("Hello")
-
-
-        optimizer = Optimizer(
-            model = create_model(total_embeddings),
-            training_rdd = train_data,
-            criterion = AbsCriterion(False),
-            optim_method = SGD(learningrate=0.01, learningrate_decay=0.0002),
-            end_trigger = MaxEpoch(10),
-            batch_size = 4)
-
-        optimizer.optimize()
 
 
 if __name__ == "__main__":
     conf=SparkConf().setAppName('test').setMaster('spark://saba-Aspire-VN7-591G:7077')
     sc = SparkContext.getOrCreate(conf)
-    entities = pd.read_table("/home/saba/Documents/Big Data Lab/data/WN18/entity2id.txt", header=None)
-    dict_entities = dict(zip(entities[0], entities[1]))
-    print(len(entities))
-    relations = pd.read_table("/home/saba/Documents/Big Data Lab/data/WN18/relation2id.txt", header=None)
-    dict_relations = dict(zip(relations[0], relations[1]))
-    print(len(relations))
+    entity2id = pd.read_table("/home/saba/Documents/Big Data Lab/data/WN18/entity2id.txt", header=None)
+    dict_entities = dict(zip(entity2id[0], entity2id[1]))
+    print(len(entity2id))
+    relation_df = pd.read_table("/home/saba/Documents/Big Data Lab/data/WN18/relation2id.txt", header=None)
+    dict_relations = dict(zip(relation_df[0], relation_df[1]))
+    print(len(relation_df))
     training_df = pd.read_table("/home/saba/Documents/Big Data Lab/data/WN18/train.txt", header=None)
     training_triples = list(zip([dict_entities[h] + 1 for h in training_df[0]],
                                  [dict_entities[t] + 1 for t in training_df[1]],
-                                  [dict_relations[r]+len(entities) + 1 for r in training_df[2]]))
+                                  [dict_relations[r]+len(entity2id) + 1 for r in training_df[2]]))
 
     transE = TransE(dict_entities, dict_relations, training_triples, margin=1, dim=50)
-    transE.generate_corrupted_triplets(1)
-    no_entities_relations=len(entities)+len(relations)
-    print("type",(no_entities_relations))
-    transE.make_samples(no_entities_relations)
-
-
-    #transE.create_embeddings()
-    #transE.createrdd(sc)
-    #transE.makeRDD(sc)
+    transE.transE(1)
+    transE.create_embeddings()
+    transE.createrdd(sc)
+    transE.makeRDD(sc)
     sc.stop()
