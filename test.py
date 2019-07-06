0216dd5abd74f778f4fb45e19a5e974b22630c0c
saba6099 <droptosabakhan@gmail.com>
2019-07-06 20:26:32 +0200

    Added evaluate functionality

--- test.py
+++ test.py
@@ -274,56 +274,14 @@ class TransE:
                              [3]],
                             [[4],
                              [5],
-                             [6]]],
-                           [[[7],
-                             [8],
-                             [9]],
-                            [[10],
-                             [11],
-                             [12]]],
-                           [[[13],
-                             [14],
-                             [15]],
-                            [[16],
-                             [17],
-                             [18]]],
-                           [[[19],
-                             [20],
-                             [21]],
-                            [[22],
-                             [23],
-                             [24]]],
-                           [[[25],
-                             [26],
-                             [27]],
-                            [[28],
-                             [29],
-                             [30]]],
-                           [[[31],
-                             [32],
-                             [33]],
-                            [[34],
-                             [35],
-                             [36]]],
-                           [[[37],
-                             [38],
-                             [39]],
-                            [[40],
-                             [41],
-                             [42]]],
-                           [[[43],
-                             [44],
-                             [45]],
-                            [[46],
-                             [47],
-                             [48]]]])
-        # test_data = JTensor.from_ndarray(sample)
-        sample_rdd = sc.parallelize(sample)
-        labels = np.zeros(8)
-        # labels=np.array([[1],[1],[1],[1]])
-        labels = sc.parallelize(labels)
-        record = sample_rdd.zip(labels)
-        test_data = record.map(lambda t: Sample.from_ndarray(t[0], t[1]))
+                             [6]]]])
+        test_data = JTensor.from_ndarray(sample)
+        # sample_rdd = sc.parallelize(sample)
+        # labels = np.zeros(8)
+        # # labels=np.array([[1],[1],[1],[1]])
+        # labels = sc.parallelize(labels)
+        # record = sample_rdd.zip(labels)
+        # test_data = record.map(lambda t: Sample.from_ndarray(t[0], t[1]))
 
         #model = self.make_samples()
 
